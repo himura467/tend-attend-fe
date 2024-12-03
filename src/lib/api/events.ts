2d09237cd976d2909d81ef763f6c060df2aa898a
himura-shitara <mitarashidango0927@gmail.com>
2024-12-04 00:32:21 +0900

    feat: getHostEvents

--- src/lib/api/events.ts
+++ src/lib/api/events.ts
@@ -1,8 +1,15 @@
 import axiosInstance from "@/lib/api/axios-instance";
-import { CreateEventRequest, CreateEventResponse } from "@/lib/api/dtos/event";
+import { CreateEventRequest, CreateEventResponse, GetHostEventsResponse } from "@/lib/api/dtos/event";
 
 export const createEvent = async (data: CreateEventRequest): Promise<CreateEventResponse> => {
   const response = await axiosInstance.post<CreateEventResponse>("/events/hosts", data, {
+    withCredentials: true,
+  });
+  return response.data;
+};
+
+export const getHostEvents = async (): Promise<GetHostEventsResponse> => {
+  const response = await axiosInstance.get<GetHostEventsResponse>("/events/hosts", {
     withCredentials: true,
   });
   return response.data;
