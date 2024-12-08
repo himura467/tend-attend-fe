417c311f2650a2d4a7d8517485fd02f13931b0ac
himura-shitara <mitarashidango0927@gmail.com>
2024-12-08 18:45:01 +0900

    feat: areEqualByRegExps

--- src/lib/utils/array.ts
+++ src/lib/utils/array.ts
@@ -12,27 +12,28 @@ export const areEqual = <T>(
   if (a === b) return true;
   if (a == null || b == null) return a === b;
   if (a.length !== b.length) return false;
+
+  const compareValues = (value: T, otherValue: T): boolean => {
+    if (Array.isArray(value) && Array.isArray(otherValue)) {
+      return areEqual(value, otherValue, options);
+    }
+    if (typeof value === "object" && typeof otherValue === "object" && value !== null && otherValue !== null) {
+      return JSON.stringify(value) === JSON.stringify(otherValue);
+    }
+    return options.strict ? value === otherValue : value == otherValue;
+  };
+
   if (options.compareOrder) {
-    return a.every((value, index) => {
-      const otherValue = b[index];
-      if (Array.isArray(value) && Array.isArray(otherValue)) {
-        return areEqual(value, otherValue, options);
-      }
-      if (typeof value === "object" && typeof otherValue === "object" && value !== null && otherValue !== null) {
-        return JSON.stringify(value) === JSON.stringify(otherValue);
-      }
-      return options.strict ? value === otherValue : value == otherValue;
-    });
+    return a.every((value, index) => compareValues(value, b[index]));
   }
-  return a.every((value) =>
-    b.some((otherValue) => {
-      if (Array.isArray(value) && Array.isArray(otherValue)) {
-        return areEqual(value, otherValue, options);
-      }
-      if (typeof value === "object" && typeof otherValue === "object" && value !== null && otherValue !== null) {
-        return JSON.stringify(value) === JSON.stringify(otherValue);
-      }
-      return options.strict ? value === otherValue : value == otherValue;
-    }),
-  );
+  return a.every((value) => b.some((otherValue) => compareValues(value, otherValue)));
+};
+
+export const areEqualByRegExps = (a: string[], b: RegExp[]): boolean => {
+  if (a.length !== b.length) return false;
+
+  return a.every((value, index) => {
+    const regExp = b[index];
+    return regExp.test(value);
+  });
 };
