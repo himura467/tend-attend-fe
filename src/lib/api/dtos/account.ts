cdbcaf9610b99fdfdac3111e9234f1b5e55bf502
himura-shitara <mitarashidango0927@gmail.com>
2024-12-13 06:33:59 +0900

    feat: dto

--- src/lib/api/dtos/account.ts
+++ src/lib/api/dtos/account.ts
@@ -7,3 +7,15 @@ export interface CreateHostAccountRequest {
 export interface CreateHostAccountResponse {
   error_codes: number[];
 }
+
+export interface CreateGuestAccountRequest {
+  guest_first_name: string;
+  guest_last_name: string;
+  guest_nickname: string | null;
+  password: string;
+  host_name: string;
+}
+
+export interface CreateGuestAccountResponse {
+  error_codes: number[];
+}
