dece51c6f797242df505934d24ac39b4327c8c53
himura-shitara <mitarashidango0927@gmail.com>
2024-12-17 11:45:27 +0900

    docs: TODO comment

--- src/lib/api/axios-instance.ts
+++ src/lib/api/axios-instance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://dubte1y7uk.execute-api.ap-northeast-1.amazonaws.com/",
+  baseURL: "https://dubte1y7uk.execute-api.ap-northeast-1.amazonaws.com/", // TODO: 環境変数を使う
   headers: {
     "Content-Type": "application/json",
   },
