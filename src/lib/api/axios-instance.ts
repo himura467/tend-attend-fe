73765d5edd12c0c16a0f6b00663aec080fbb86b0
himura-shitara <mitarashidango0927@gmail.com>
2024-12-17 11:39:15 +0900

    feat: hard coding

--- src/lib/api/axios-instance.ts
+++ src/lib/api/axios-instance.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BACKEND_API_URL || "http://localhost:8000",
+  baseURL: "https://dubte1y7uk.execute-api.ap-northeast-1.amazonaws.com/",
   headers: {
     "Content-Type": "application/json",
   },
