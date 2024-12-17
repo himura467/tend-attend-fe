ef472965198727bb076b76ec50e0ec4cc6c5370e
himura-shitara <mitarashidango0927@gmail.com>
2024-12-17 12:14:37 +0900

    feat: Access-Control-Allow-Origin header

--- src/lib/api/axios-instance.ts
+++ src/lib/api/axios-instance.ts
@@ -3,6 +3,9 @@ import axios from "axios";
 const axiosInstance = axios.create({
   baseURL: "https://dubte1y7uk.execute-api.ap-northeast-1.amazonaws.com/", // TODO: 環境変数を使う
   headers: {
+    "Access-Control-Allow-Origin": "*", // TODO: ガバガバすぎるのでいずれなんとかする
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
     "Content-Type": "application/json",
   },
 });
