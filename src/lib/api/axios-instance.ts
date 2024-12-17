53c1547770e8c32b324cc103264c6a986ebdc2fc
himura-shitara <mitarashidango0927@gmail.com>
2024-12-17 13:08:41 +0900

    fix: add forgotten stage to baseURL

--- src/lib/api/axios-instance.ts
+++ src/lib/api/axios-instance.ts
@@ -1,11 +1,11 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
-  baseURL: "https://dubte1y7uk.execute-api.ap-northeast-1.amazonaws.com/", // TODO: 環境変数を使う
+  baseURL: "https://dubte1y7uk.execute-api.ap-northeast-1.amazonaws.com/prod/", // TODO: 環境変数を使う
   headers: {
     "Access-Control-Allow-Origin": "*", // TODO: ガバガバすぎるのでいずれなんとかする
-    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
-    "Access-Control-Allow-Headers": "Content-Type, Authorization",
+    "Access-Control-Allow-Methods": "*",
+    "Access-Control-Allow-Headers": "*",
     "Content-Type": "application/json",
   },
 });
