87b9296c5f2c6fa8f30f84c9fd01a6308f9c810b
himura467 <mitarashidango0927@gmail.com>
2024-12-20 18:12:01 +0900

    feat: x-api-key

--- src/lib/api/axios-instance.ts
+++ src/lib/api/axios-instance.ts
@@ -4,6 +4,7 @@ const axiosInstance = axios.create({
   baseURL: process.env.NEXT_PUBLIC_BACKEND_API_URL,
   headers: {
     "Content-Type": "application/json",
+    "x-api-key": process.env.NEXT_PUBLIC_API_GATEWAY_API_KEY,
   },
 });
 
