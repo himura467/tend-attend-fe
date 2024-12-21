51bc9516f73e21476157522325d933a2a4551029
himura467 <mitarashidango0927@gmail.com>
2024-12-21 09:30:27 +0900

    feat: BACKEND_API_URL

--- next.config.mjs
+++ next.config.mjs
@@ -1,6 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   env: {
+    BACKEND_API_URL: process.env.BACKEND_API_URL,
     API_GATEWAY_API_KEY: process.env.API_GATEWAY_API_KEY,
   },
 };
