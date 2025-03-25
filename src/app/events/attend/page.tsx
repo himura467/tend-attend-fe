7cead36540e03528d3472346c956b0a804da5a0e
himura467 <113117363+himura467@users.noreply.github.com>
2025-03-26 04:34:52 +0900

    イベント編集ページとイベント出席ページに相互リンクを張る (#79)

--- src/app/events/attend/page.tsx
+++ src/app/events/attend/page.tsx
@@ -1,13 +1,19 @@
 import { DialogTemplate } from "@/components/templates/DialogTemplate";
 import { NextPage } from "next";
 import { EventAttendanceCalendarForm } from "@/components/organisms/specific/events/attend/EventAttendanceCalendarForm";
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+import { rr } from "@/lib/utils/reverse-router";
 
 const AttendEventPage: NextPage = (): React.JSX.Element => {
   return (
     <DialogTemplate>
       <div className="w-full space-y-6">
-        <div className="text-center">
+        <div className="flex items-center justify-between">
           <h1 className="text-3xl font-bold tracking-tight text-foreground sm:text-4xl">Attend events</h1>
+          <Button variant="outline" asChild>
+            <Link {...rr.events.edit.index()}>Edit Events</Link>
+          </Button>
         </div>
         <EventAttendanceCalendarForm />
       </div>
