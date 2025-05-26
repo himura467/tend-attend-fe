2884eec5adec468bd6f709d082cc0b8f404b8a84
himura467 <mitarashidango0927@gmail.com>
2025-05-26 23:03:49 +0900

    feat: next env

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,10 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  env: {
+    BACKEND_API_URL: process.env.BACKEND_API_URL,
+    API_GATEWAY_API_KEY: process.env.API_GATEWAY_API_KEY,
+  },
 };
 
 export default nextConfig;
